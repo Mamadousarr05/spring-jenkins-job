9a320e58dc0f63965ce6ec0542ab7d6576c39654
Mamadousarr05 <mausarr05@gmail.com.com>
2023-02-15 10:38:17 +0000

    teste pipline

--- src/test/java/com/sarr/springjenkinsjob/SpringJenkinsJobApplicationTests.java
+++ src/test/java/com/sarr/springjenkinsjob/SpringJenkinsJobApplicationTests.java
@@ -13,7 +13,6 @@ class SpringJenkinsJobApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing....");
-		logger.info("Test case executing second log statement....");
 
 		assertEquals(true, true);
 	}
